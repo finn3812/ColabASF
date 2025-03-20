bbb180b25ad75cc1e393bc40f61d92d2b96bcadc
Hotdoghvam07 <155617376+Hotdoghvam07@users.noreply.github.com>
2025-03-20 12:15:31 +0100

    Tilføjelse af Movement via WASD til Spiller Placeholder

--- Assets/Undersystemmer/PlayerControl/scripts/Player Controls.cs
+++ Assets/Undersystemmer/PlayerControl/scripts/Player Controls.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f; // Bevægelseshastighed
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); // Henter Rigidbody-komponenten
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Input fra WASD-tasterne
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D eller venstre/højre pile
+        float moveVertical = Input.GetAxis("Vertical"); // W/S eller op/ned pile
+
+        // Beregn bevægelsesretningen
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Flyt objektet
+        rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
     }
 }
