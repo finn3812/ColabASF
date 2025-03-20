642ea93b266e1e0baef69b94595c04f907660b95
Niklas Schulz <144591117+niklas-schulz@users.noreply.github.com>
2025-03-20 12:42:08 +0100

    NPC Simpel funktions implemtation, samt begyndelse på AI

--- Assets/Undersystemmer/NPCControl/scripts/NPC.cs
+++ Assets/Undersystemmer/NPCControl/scripts/NPC.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public interface INPCState
 {
@@ -9,18 +10,31 @@ public interface INPCState
     void Update(NPC NPC) { }
 }
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class NPC : MonoBehaviour
 {
+    NavMeshAgent agent;
     INPCState currentState;
-
+    
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        NPCStart();
+    }
+
+    protected virtual void NPCStart()
+    {
+
     }
 
     void Update()
     {
         currentState?.Update(this);
+    }
+
+    protected virtual void EventHandler()
+    {
+
     }
 
     void TransitionState(INPCState state)
