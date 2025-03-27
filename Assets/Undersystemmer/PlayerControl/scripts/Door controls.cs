87f92f0298a8f3b04b4b37d11cb80163e9916cba
Hotdoghvam07 <155617376+Hotdoghvam07@users.noreply.github.com>
2025-03-27 12:54:13 +0100

    Dør bruger nu en collider

--- Assets/Undersystemmer/PlayerControl/scripts/Door controls.cs
+++ Assets/Undersystemmer/PlayerControl/scripts/Door controls.cs
@@ -5,44 +5,66 @@ using static UnityEditor.VersionControl.Asset;
 
 public class Doorcontrols : MonoBehaviour
 {
-    public enum state { idle, DoorOpen, DoorClose }
-    public state states = state.idle;
+    public enum State { Idle, DoorOpen, DoorClose }
+    public State states = State.Idle;
 
-    public CharacterController controller;
-    Animator animator;
-    // Start is called before the first frame update
+    public CharacterController controller; // Not strictly needed for proximity checks
+    private Animator animator;
+    private bool playerInRange = false; // To track if the player is within range
+
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!playerInRange)
+        {
+            return; // Ignore input if the player is not in range
+        }
+
         switch (states)
         {
-            case state.idle:
+            case State.Idle:
                 animator.SetInteger("state", 0);
                 if (Input.GetKeyDown(KeyCode.O))
                 {
-                    states = state.DoorOpen;
+                    states = State.DoorOpen;
                 }
                 break;
-            case state.DoorOpen:
+
+            case State.DoorOpen:
                 animator.SetInteger("state", 1);
                 if (Input.GetKeyDown(KeyCode.C))
                 {
-                    states = state.DoorClose;
+                    states = State.DoorClose;
                 }
-
                 break;
-            case state.DoorClose:
+
+            case State.DoorClose:
                 animator.SetInteger("state", 2);
                 if (Input.GetKeyDown(KeyCode.O))
                 {
-                    states = state.DoorOpen;
+                    states = State.DoorOpen;
                 }
                 break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Assuming the player has the tag "Player"
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
         }
     }
 }
