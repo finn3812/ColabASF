550325bf7858337f674050d8799c69cb2d35b417
Mjarkars <evapaludan@outlook.com>
2025-03-27 13:06:09 +0100

    Started Camera Script

--- Assets/Undersystemmer/DoorCamMap/scripts/Overvaagning.cs
+++ Assets/Undersystemmer/DoorCamMap/scripts/Overvaagning.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Overvaagning : MonoBehaviour
 {
+    private List<Camera> cameraList = new List<Camera>();
     // Start is called before the first frame update
     void Start()
     {
-        
+   //     Camera[] cameras = GameObject.FindGameObjectsWithTag("VideoCamera").Select(obj => obj.GetComponent<Camera>()).ToArray();
+    //    cameraList.AddRange(cameras);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
