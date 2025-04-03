fd0809d99d3dda34f8ce979c6e9039503105853d
Hotdoghvam07 <155617376+Hotdoghvam07@users.noreply.github.com>
2025-04-03 12:28:08 +0200

    Har nu oprettet varianter af camera systemet til spiller tests

---
+++ Assets/Undersystemmer/PlayerControl/scripts/Overvaagning2.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overvaagning2 : MonoBehaviour
+{
+	public List<GameObject> cameraList = new List<GameObject>();
+
+	public RawImage img;
+	public RenderTexture renderTexture;
+
+	private Camera currentCamera;
+	private int currentCameraIndex = 0;
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameObject[] cameras = GameObject.FindGameObjectsWithTag("VideoCamera");
+		foreach (GameObject cam in cameras)
+		{
+			cameraList.Add(cam);
+			cam.SetActive(false);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		{
+
+			if (Input.GetKeyDown(KeyCode.M))
+			{
+				currentCameraIndex++;
+
+				if (currentCameraIndex >= cameraList.Count)
+				{
+					currentCameraIndex = 0;
+				}
+
+				changecamtotexture(currentCameraIndex);
+			}
+		}
+	}
+
+
+
+	public void changecamtotexture(int camNumber)
+	{
+		if (camNumber >= 0 && camNumber < cameraList.Count)
+		{
+			if(currentCamera != null)
+			{
+				currentCamera.gameObject.SetActive(false);
+			}
+			
+			Camera activeCamera = cameraList[camNumber].GetComponent<Camera>();
+			if (activeCamera.gameObject != null)
+			{
+				activeCamera.gameObject.SetActive(true);	
+			}
+
+			
+			if (currentCamera != null)
+			{
+				currentCamera.targetTexture = null;
+			}
+
+			
+			activeCamera.targetTexture = renderTexture;
+			img.texture = renderTexture;
+			currentCamera = activeCamera;
+		}
+		else
+		{
+			Debug.LogWarning("Camera number is out of range.");
+		}
+	}
+
+}
