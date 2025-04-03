1c36767086061ddd234ef27575ef35f8082391c5
Mjarkars <evapaludan@outlook.com>
2025-04-03 11:50:11 +0200

    Startede overvågnings script

--- Assets/Undersystemmer/DoorCamMap/scripts/Overvaagning.cs
+++ Assets/Undersystemmer/DoorCamMap/scripts/Overvaagning.cs
@@ -1,20 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Overvaagning : MonoBehaviour
 {
-    private List<Camera> cameraList = new List<Camera>();
-    // Start is called before the first frame update
-    void Start()
-    {
-   //     Camera[] cameras = GameObject.FindGameObjectsWithTag("VideoCamera").Select(obj => obj.GetComponent<Camera>()).ToArray();
-    //    cameraList.AddRange(cameras);
-    }
+	public List<GameObject> cameraList = new List<GameObject>();
+	//public Texture NewTexture;
+	public RawImage img;
+	public RenderTexture renderTexture;
 
-    // Update is called once per frame
-    void Update()
-    {
+	private Camera currentCamera;
+	private int currentCameraIndex = 0;
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameObject[] cameras = GameObject.FindGameObjectsWithTag("VideoCamera");
+		foreach (GameObject cam in cameras)
+		{
+			cameraList.Add(cam);
+		}
 
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		{
+
+			if (Input.GetKeyDown(KeyCode.C))
+			{
+				currentCameraIndex++;
+
+				if (currentCameraIndex >= cameraList.Count)
+				{
+					currentCameraIndex = 0;
+				}
+
+				changecamtotexture(currentCameraIndex);
+			}
+		}
+	}
+
+
+
+	public void changecamtotexture(int camNumber)
+	{
+		if (camNumber >= 0 && camNumber < cameraList.Count)
+		{
+			Camera activeCamera = cameraList[camNumber].GetComponent<Camera>();
+
+			
+			if (currentCamera != null)
+			{
+				currentCamera.targetTexture = null;
+			}
+
+			
+			activeCamera.targetTexture = renderTexture;
+			img.texture = renderTexture;
+			currentCamera = activeCamera;
+		}
+		else
+		{
+			Debug.LogWarning("Camera number is out of range.");
+		}
+	}
+
 }
