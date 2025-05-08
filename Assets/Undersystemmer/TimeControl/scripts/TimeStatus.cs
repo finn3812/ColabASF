e8f7ad772e974a57d34ca5e8d195635f5170cc3d
Niklas Schulz <144591117+niklas-schulz@users.noreply.github.com>
2025-05-08 12:00:01 +0200

    Main scene update

--- Assets/Undersystemmer/TimeControl/scripts/TimeStatus.cs
+++ Assets/Undersystemmer/TimeControl/scripts/TimeStatus.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class TimeStatus : MonoBehaviour
 {
     public TMP_Text clockText;
-    bool IsStatusActive=false;
+    bool IsStatusActive=true;
     // Start is called before the first frame update
     void Start()
     {
